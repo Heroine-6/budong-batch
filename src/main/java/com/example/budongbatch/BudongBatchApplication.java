a9e958255cb391ed3505247f414fef3c06c78f1b
castlejuyeon <jucess1106@naver.com>
2026-02-03 15:36:18 +0900

    chore: @EnableFeignClients추가

--- src/main/java/com/example/budongbatch/BudongBatchApplication.java
+++ src/main/java/com/example/budongbatch/BudongBatchApplication.java
@@ -2,8 +2,10 @@ package com.example.budongbatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BudongBatchApplication {
 
     public static void main(String[] args) {
